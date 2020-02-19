705076c6ec872f45fa1fb186eb818de18c01bee7
Andrey Borovilov <simple_like@list.ru>
2020-02-19 10:20:36 +0300

    Initial Commit

---
+++ SwiftBook_7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftBook_7
+//
+//  Created by Andrey Borovilov on 19/02/2020.
+//  Copyright © 2020 Andrey Borovilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
